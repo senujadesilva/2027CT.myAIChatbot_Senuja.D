0487aa2f4015a81379c1da21717ba68bf7c8c78f
Senuja D <senuja.desilva@education.nsw.gov.au>
2026-02-23 23:17:20 +0000

    feature: add Bootstrap chat interface

--- app.py
+++ app.py
@@ -15,32 +15,6 @@ chatbot = ChatBot(
 # Train the chatbot with English conversations
 trainer = ChatterBotCorpusTrainer(chatbot)
 trainer.train("chatterbot.corpus.english")
-# Custom training for school-related topics
-from chatterbot.trainers import ListTrainer
-
-list_trainer = ListTrainer(chatbot)
-
-# Train on school-related conversations
-list_trainer.train(
-    [
-        "What subjects do you like?",
-        "I find all subjects interesting, but I really enjoy helping with coding!",
-        "Can you help with homework?",
-        "I can try to help explain concepts, but you should do your own work!",
-        "Who made you?",
-        "I was created by a talented Year 9 student at Tempe High School!",
-    ]
-)
-
-# Train on greetings
-list_trainer.train(
-    [
-        "Good morning!",
-        "Good morning! How can I help you today?",
-        "Good afternoon!",
-        "Good afternoon! What would you like to chat about?",
-    ]
-)
 
 
 @app.route("/")
